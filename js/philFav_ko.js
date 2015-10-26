e6b60528cef22e449a07cbd9a3009a1af17e07e0
dingGator <kim@chucrew.net>
2015-10-26 17:33:47 -0400

    Add search box
    Added search box but it does not work.

--- js/philFav_ko.js
+++ js/philFav_ko.js
@@ -65,15 +65,30 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 
-var input = document.getElementById('pac-input');
-var searchBox = new google.maps.places.SearchBox(input);
+};
+};
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.9500, lng: -75.1667},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
-    var markers = [];
+
+  var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
@@ -82,10 +97,11 @@ var searchBox = new google.maps.places.SearchBox(input);
     if (places.length == 0) {
       return;
     }
-//     Clear out the old markers.
-   markers.forEach(function(marker) {
-    marker.setMap(null);
-  });
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
     markers = [];
 
     // For each place, get the icon, name and location.
@@ -116,8 +132,7 @@ var searchBox = new google.maps.places.SearchBox(input);
     });
     map.fitBounds(bounds);
   });
-};
-};
+}
 var viewModel = {
 	philaFav: ko.observableArray([])
 
