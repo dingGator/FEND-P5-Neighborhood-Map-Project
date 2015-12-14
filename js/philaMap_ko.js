2b6602acdf8311a6549ec850f0bf434d125129ce
dingGator <kim@chucrew.net>
2015-12-14 10:26:07 -0500

    Buttons added
    Button added

--- js/philaMap_ko.js
+++ js/philaMap_ko.js
@@ -1,40 +1,90 @@
 var myLocation = "";
+var quickFacts = "";
 
 var philaFav = [
-	{ "myLocation":"National Constitution Center",
-		name:'National Constitution Center',
+	{
+    "myLocation":"National Constitution Center",
+		name:'NATIONAL CONSTITUTION CENTER '+
+    ' \n Quick Facts: The National Constitution Center '+
+    ' \n is a nonprofit, nonpartisan institution devoted'+
+    ' \n to Disnifying the United States Constitution and'+
+    ' \n what it represents.Wikipedia',
 		lat:39.953904,
 		lng:-75.149066,
-	},
-	{ "myLocation":"Philadelphia Museum of Art",
-		name:'Philadelphia Museum of Art',
-		lat: 39.965590,
+  },
+	{
+    "myLocation":"Philadelphia Museum of Art",
+		name:'PHILADELPHIA MUSEUM OF ART'+
+    ' \n Quick Facts: The Philadelphia Museum of Art '+
+    ' \n It has collections of more than 227,000 objects that '+
+    ' \n include world-class holdings of European and '+
+    ' \n American paintings, prints, drawings, and decorative '+
+    ' \n arts.Wikipedia ',
+
+    lat: 39.965590,
 		lng: -75.180998,
+
+
 	},
-	{ "myLocation":"Independence Hall",
-		name:'Independence Hall',
+	{
+    "myLocation":"Independence Hall",
+		name:'INDEPENDENCE HALL'+
+    ' \n Quick Facts: Independence National Historical Park'+
+    ' \n is a United States National Park in Philadelphia that'+
+    ' \n preserves several sites associated with the' +
+    ' \n American Revolution and the nation founding history.Wikipedia'+
+    ' \n Area:40 acres, Architect:William Strickland,' +
+    ' \n Established:July 4, 1956',
 		lat: 39.94880,
 		lng: -75.150068,
+
 	},
-	{ "myLocation":"Franklin Square",
-		name:'Franklin Square',
-		lat: 39.9555,
+	{
+    "myLocation":"Franklin Square",
+		name:'FRANKLIN SQUARE' +
+    ' \n Quick Facts": Franklin Square is one of the five' +
+    ' \n original open-space parks'+
+    ' \n planned by William Penn when he laid out the'+
+    ' \n city of Philadelphia, Pennsylvania in 1682.Wikipedia'+
+    ' \n Area:8 acres, Year built:1683, Architect:Thomas Holme',
+
+    lat: 39.9555,
 		lng: -75.1508,
+
 	},
-	{ "myLocation":"Liberty Bell",
-		name:'Liberty Bell',
+	{
+    "myLocation":"Liberty Bell",
+		name:'LIBERTY BELL' +
+    ' \n Quick Facts": "The Liberty Bell is an iconic symbol of' +
+    ' \n American independence, located in Philadelphia, Pennsylvania.'+
+    ' \n Wikipedia, Owners:Philadelphia, Materials:CopperTin,'+
+    ' \n Municipality:Philadelphia',
 		lat: 39.949702,
 		lng: -75.150219,
+
 	},
-	{ "myLocation":"Reading Terminal Market",
-		name:'Reading Terminal Market',
-		lat: 39.952614,
+	{
+    "myLocation":"Reading Terminal Market",
+		name:'READING TERMINAL MARKET' +
+		' \n Quick Facts: The Reading Terminal is a complex of' +
+    ' \n buildings and former railroad station located in the Market'+
+    ' \n last section of Center City in the city of Philadelphia,'+
+    ' \n Pennsylvania, United States.Wikipedia'+
+    ' \n Area:4 acres, Architect:Francis H. Kimball',
+    lat: 39.952614,
 		lng: -75.159444,
+
 	},
-	{  "myLocation":"Chinatown",
-		name:'Chinatown',
-		lat: 39.954568,
+	{
+    "myLocation":"Chinatown",
+		name:'CHINATOWN'+
+		' \n Quick Facts": "Philadelphia Chinatown is a predominantly' +
+    ' \n Asian American neighborhood in Center City Philadelphia.'+
+    ' \n The Philadelphia Chinatown Development Corporation supports' +
+    ' \n the area.Wikipedia',
+    lat: 39.954568,
 		lng: -75.156084,
+
 	},
 
 
@@ -56,6 +106,7 @@ for (var i =0; i< philaFav.length; i++) {
 };
 
 var infowindow;
+
 function createMarker(place) {
   	var placeLoc = place.geometry.location;
   	var marker = new google.maps.Marker({
@@ -64,78 +115,14 @@ function createMarker(place) {
   });
 
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
+    infowindow.setContent(name);
     infowindow.open(map, this);
   });
 
+
 };
+
 };
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.9520, lng: -75.1566},
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
 var viewModel = function() {
   var self = this;
   self.points = ko.observableArray(philaFav);
